dd98db95a4a911e9dc347b5397af87d6e89f6406
dlarionov <dlarionov@belta.by>
2024-09-13 17:57:26 +0300

    Дополнение

--- internal/modules/admin/admin.route.go
+++ internal/modules/admin/admin.route.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-func registerRoute(path string, handler func(*AdminController)) {
+func registerRoute(path string, handler func(controller *AdminPageController)) {
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		//nc := NewAdminController(w, r)
 		//handler(nc)
@@ -14,4 +14,5 @@ func registerRoute(path string, handler func(*AdminController)) {
 func RegisterRoutes() {
 	//registerRoute("/admin/tree", (*AdminController).Load)
 	//registerRoute("/admin1", (*AdminController).Load)
+	//registerRoute("/admin_createModule", (*AdminController).CreateModule)
 }
